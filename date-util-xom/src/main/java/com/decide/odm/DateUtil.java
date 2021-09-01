ea283b93428e3aab88144258802bb5457b90d488
eduizquierdo <eduardo.izquierdo@decidesoluciones.es>
2021-09-01 13:49:10 +0200

    Second commit

--- date-util-xom/src/main/java/com/decide/odm/DateUtil.java
+++ date-util-xom/src/main/java/com/decide/odm/DateUtil.java
@@ -1,12 +1,16 @@
 package com.decide.odm;
 
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.time.temporal.ChronoUnit;
 import java.util.Date;
 
 public class DateUtil {
 
-    public static int DaysBetween(Date d1, Date d2) {
-
-        return 0;
+    public static long DaysBetween(Date d1, Date d2) {
+        LocalDate local1 = d1.toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
+        LocalDate local2 = d2.toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
+        return ChronoUnit.DAYS.between(local1, local2);
     }
 
     public DateUtil() {
