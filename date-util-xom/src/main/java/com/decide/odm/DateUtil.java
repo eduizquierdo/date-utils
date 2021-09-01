809c54d9688c6f10570bb62d05a73692e24c6482
eduizquierdo <eduardo.izquierdo@decidesoluciones.es>
2021-09-01 17:10:47 +0200

    Added launch configuration

--- date-util-xom/src/main/java/com/decide/odm/DateUtil.java
+++ date-util-xom/src/main/java/com/decide/odm/DateUtil.java
@@ -1,5 +1,7 @@
 package com.decide.odm;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.time.LocalDate;
 import java.time.ZoneId;
 import java.time.temporal.ChronoUnit;
@@ -8,7 +10,9 @@ import java.util.Date;
 
 public class DateUtil {
 
-    public static long DaysBetween(Date d1, Date d2) {
+    private static final SimpleDateFormat FORMAT = new SimpleDateFormat("yyy-MM-dd");
+
+    public static long getDaysBetween(Date d1, Date d2) {
         LocalDate local1 = d1.toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
         LocalDate local2 = d2.toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
         return ChronoUnit.DAYS.between(local1, local2);
@@ -16,6 +20,30 @@ public class DateUtil {
 
     public static Date getDateOfToday() {
         return Calendar.getInstance().getTime();
+    }
+
+    public static String getDateAsString(Date d) {
+        return FORMAT.format(d);
+    }
+
+    public static Date getDateFromString(String str) throws ParseException {
+        return FORMAT.parse(str);
+    }
+
+    public static Date addDays(Date d, int days) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(d);
+        calendar.add(Calendar.DAY_OF_MONTH, days);
+        return calendar.getTime();
+    }
+
+    public static void main(String[] args) throws ParseException {
+        Date d1 = DateUtil.getDateFromString("2021-09-01");
+        Date d2 = DateUtil.getDateFromString("2021-09-28");
+        System.out.println(getDateAsString(d1) + " + 5 days = " + getDateAsString(addDays(d1, 5)));
+        System.out.println(
+                "days between " + getDateAsString(d1) + " and " + getDateAsString(d2) + " = " + getDaysBetween(d1, d2));
+
     }
 
 }
